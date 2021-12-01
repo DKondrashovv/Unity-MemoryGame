7de3d9a4df2a03cbf421ca90812eb381e97046db
KonDan9 <danik.kondrashov.8@gmail.com>
2021-12-01 14:07:28 +0300

    Add MemoryGame

---
+++ Assets/Scripts/timeScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class timeScript : MonoBehaviour {
+
+	public Text counterText;
+	public bool timeCounter = true;
+	public float seconds;
+
+	
+	void Start () {
+		counterText = GetComponent<Text> () as Text;
+	}
+	
+
+	void Update () {
+		if (timeCounter) {
+			seconds = (int)(Time.timeSinceLevelLoad % 60f);
+			counterText.text = seconds.ToString ("00");
+		}
+		
+	}
+
+	public void endGame() {
+		timeCounter = false;
+		counterText.color = Color.red;
+	}
+}
